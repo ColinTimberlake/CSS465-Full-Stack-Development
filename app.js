f9ec82d82c16e928ed8cde9044fc57e52c4928c5
Colin <colint1993@yahoo.com>
2024-02-25 18:40:10 -0600

    Website Update, angular and security

--- app.js
+++ app.js
@@ -60,6 +60,11 @@ app.use("/about", aboutRouter);
 
 app.use("/api", apiRouter);
 
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
